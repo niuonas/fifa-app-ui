009228ff229d6ebb4bf19f41dbbaac8b448cad93
Nicu Iuonas <nicu2595@gmail.com>
2022-12-01 14:25:36 +0200

    Created players module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayersComponent } from './players/players.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayersComponent
   ],
   imports: [
     BrowserModule,
