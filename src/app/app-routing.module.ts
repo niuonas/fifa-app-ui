35d6f2b0abbaf4d92aed9897e508434c4f5848b1
Nicu Iuonas <nicu2595@gmail.com>
2022-12-16 22:10:41 +0200

    Added component for teams and menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PlayersPageComponent } from './players/players-page/players.component';
+import { TeamsPageComponent } from './teams/teams-page/teams-page.component';
 
 const routes: Routes = [
   {
+    path: 'players',
+    component: PlayersPageComponent,
+  },
+  {
+    path: 'teams',
+    component: TeamsPageComponent,
+  },
+  {
     path: '',
-    redirectTo: 'players',
-    pathMatch: 'full',
+    component: HomePageComponent,
   },
 ];
 
