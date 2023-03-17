4152a350f96df7cc54c16d2c039a6f3e08a73018
Nicu Iuonas <nicu2595@gmail.com>
2023-03-17 21:59:20 +0200

    Add player to team

--- src/app/teams/teams-page/teams-page.component.ts
+++ src/app/teams/teams-page/teams-page.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { PlayerDialogComponent } from './../../players/components/player-dialog/player-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { Team } from '../models/team.model';
+import { TeamService } from '../service/team.service';
+import { Player } from 'src/app/players/models/player.model';
 
 @Component({
   selector: 'app-teams-page',
   templateUrl: './teams-page.component.html',
-  styleUrls: ['./teams-page.component.css']
+  styleUrls: ['./teams-page.component.css'],
 })
-export class TeamsPageComponent {
+export class TeamsPageComponent implements OnInit, OnDestroy {
+  constructor(private teamService: TeamService, public dialg: MatDialog) {}
 
+  player: Player = {} as Player;
+  displayedColumns: string[] = ['Name', 'Players'];
+  teams$: Observable<Team[]> = new Observable();
+  selectedTeam: Team = {} as Team;
+  subscription: Subscription = {} as Subscription;
+
+  ngOnInit(): void {
+    this.teams$ = this.teamService.getTeams$();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  openDialog(team: Team): void {
+    this.selectedTeam = team;
+    const dialogRef = this.dialg.open(PlayerDialogComponent, {
+      width: '250px',
+      data: {
+        player: this.player,
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.addPlayerToTeam(result);
+      }
+    });
+  }
+
+  addPlayerToTeam(player: Player) {
+    this.subscription = this.teamService
+      .addPlayerToTeam$(player, this.selectedTeam.id)
+      .subscribe(() => {
+        (this.teams$ = this.teamService.getTeams$()),
+          (this.player = {} as Player);
+      });
+  }
 }
